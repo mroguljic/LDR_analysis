83ea1864ab2b4dd3681416f8e43286287c2014db
mroguljic <matej.roguljic@gmail.com>
2019-11-26 10:14:47 +0100

    Added time delay plots

--- chipsComparison.py
+++ chipsComparison.py
@@ -53,6 +53,50 @@ def getCorrectedRO(inputCSV):
 
   return inverterClk,inverter,nand,nor,doses,temperatures
 
+
+def getTimeDelays(inputCSV):
+  normalization = 1.
+  with open(inputCSV) as csv_file:
+    inverter0   =[]
+    inverter4   =[]
+    clock0      =[]
+    clock4      =[]
+    notand0     =[]
+    notand4     =[]
+    notor0      =[]
+    notor4      =[]
+    doses       =[]
+    temperatures=[]
+    nominalRow  = findNominalValues(inputCSV)
+    csv_reader  = csv.reader(csv_file, delimiter=',',quoting=csv.QUOTE_NONNUMERIC)
+    for row in csv_reader:
+        timestamp = row[0]
+        temp      = row[1]
+        dose      = row[2]
+        clk0      = (normalization*(1/row[3])/(1/nominalRow[3]) - 1)*100
+        clk4      = (normalization*(1/row[4])/(1/nominalRow[4]) - 1)*100
+        inv0      = (normalization*(1/row[5])/(1/nominalRow[5]) - 1)*100
+        inv4      = (normalization*(1/row[6])/(1/nominalRow[6]) - 1)*100
+        nand0     = (normalization*(1/row[7])/(1/nominalRow[7]) - 1)*100
+        nand4     = (normalization*(1/row[8])/(1/nominalRow[8]) - 1)*100
+        nor0      = (normalization*(1/row[9])/(1/nominalRow[9]) - 1)*100
+        nor4      = (normalization*(1/row[10])/(1/nominalRow[10]) - 1)*100
+
+        clock0.append(clk0)
+        clock4.append(clk4)
+        inverter0.append(inv0)
+        inverter4.append(inv4)
+        notand0.append(nand0)
+        notand4.append(nand4)
+        notor0.append(nor0)
+        notor4.append(nor4)
+        temperatures.append(temp)
+        doses.append(dose)
+
+  return clock0,clock4,inverter0,inverter4,notand0,notand4,notor0,notor4,doses,temperatures
+
+
+
 def getRawRO(inputCSV):
   normalization = 1.
   with open(inputCSV) as csv_file:
@@ -202,19 +246,55 @@ def plotRawRO(driverPosition=0, outputFile='rawPlot.png',windowLength=5,markersi
   plt.close()
 
 
-plotRawRO(0,'clk0.png')
-plotRawRO(1,'clk4.png')
-plotRawRO(2,'inv0.png')
-plotRawRO(3,'inv4.png')
-plotRawRO(4,'not0.png')
-plotRawRO(5,'not4.png')
-plotRawRO(6,'nor0.png')
-plotRawRO(7,'nor4.png')
+def plotTimeDelays(driverPosition=0, outputFile='rawPlot.png',windowLength=5,markersize=9):
+  rawC1Data = getTimeDelays("Chip1_filtered.csv")
+  rawC2Data = getTimeDelays("Chip2_filtered.csv")
+  rawC3Data = getTimeDelays("Chip3_filtered.csv")
+  rawC4Data = getTimeDelays("Chip4_filtered.csv")
+  rawC6Data = getTimeDelays("Chip6_filtered.csv")
 
-plotCorrectedRO(0,'clk.png')
-plotCorrectedRO(1,'inv.png')
-plotCorrectedRO(2,'nand.png')
-plotCorrectedRO(3,'nor.png')
+  c1Temp=round(np.average(rawC1Data[-1]))
+  c2Temp=round(np.average(rawC2Data[-1]))
+  c3Temp=round(np.average(rawC3Data[-1]))
+  c4Temp=round(np.average(rawC4Data[-1]))
+  c6Temp=round(np.average(rawC6Data[-1]))
+
+
+  markersize=9
+
+  axes = plt.gca()
+  plt.xlabel("Dose[Mrad(SiO2)]", weight='bold',fontsize=12)
+  plt.ylabel("Normalized RO time delay percentage change",weight='bold',fontsize=12)
+  #axes.set_xlim([xmin,xmax])
+  #axes.set_ylim([98,105])
+  axes.set_xlim([-0.5,35])
+  plt.grid(b=True, which='major', axis='both')
+  plt.text(3.,99.,'Average chip temperature\n in the legend label',fontsize=12, weight='bold')
+  plt.plot(rawC1Data[-2][windowLength:-windowLength:], np.convolve(rawC1Data[driverPosition],np.ones(windowLength)/windowLength,mode='same')[windowLength:-windowLength:], 'd',markersize=markersize, markerfacecolor='none', label=str(c1Temp)+str(' deg C'))
+  plt.plot(rawC2Data[-2][windowLength:-windowLength:], np.convolve(rawC2Data[driverPosition],np.ones(windowLength)/windowLength,mode='same')[windowLength:-windowLength:], 'o',markersize=markersize, markerfacecolor='none', label=str(c2Temp)+str(' deg C'))
+  plt.plot(rawC3Data[-2][windowLength:-windowLength:], np.convolve(rawC3Data[driverPosition],np.ones(windowLength)/windowLength,mode='same')[windowLength:-windowLength:], 'v',markersize=markersize, markerfacecolor='none', label=str(c3Temp)+str(' deg C'))
+  #plt.plot(rawC4Data[-2][windowLength:-windowLength:], np.convolve(rawC4Data[driverPosition],np.ones(windowLength)/windowLength,mode='same')[windowLength:-windowLength:], 'v',markersize=markersize, label=str(c4Temp)+str(' deg C'))
+  #plt.plot(rawC6Data[-2][windowLength:-windowLength:], np.convolve(rawC6Data[driverPosition],np.ones(windowLength)/windowLength,mode='same')[windowLength:-windowLength:], 'v',markersize=markersize, label=str(c6Temp)+str(' deg C'))
+  plt.legend(loc='best')
+  plt.savefig(outputFile,dpi=200)
+  plt.close()
+
+
+# plotRawRO(0,'clk0.png')
+# plotRawRO(1,'clk4.png')
+# plotRawRO(2,'inv0.png')
+# plotRawRO(3,'inv4.png')
+# plotRawRO(4,'not0.png')
+# plotRawRO(5,'not4.png')
+# plotRawRO(6,'nor0.png')
+# plotRawRO(7,'nor4.png')
+
+# plotCorrectedRO(0,'clk.png')
+# plotCorrectedRO(1,'inv.png')
+# plotCorrectedRO(2,'nand.png')
+# plotCorrectedRO(3,'nor.png')
+
+plotTimeDelays(0,'clk0.png')
 
 
 #plotCorrectedRO(0,'clk0.png')
--- linearFit.py
+++ linearFit.py
@@ -143,10 +143,179 @@ def linearFitCorrected(ROpos,outputFile,title,skipFirstN=400):
     plt.savefig(outputFile,dpi=200)
 
 
-correctedTitles=["clk","inv","nand","nor"]
-for i, title in enumerate(correctedTitles):
-    linearFitCorrected(i,"fitsRoom/"+title+".png",title)
+def find10Mrad(data):
+
+    if(sorted(data) != data):
+        print("Data not sorted!")
+        return 0
+
+    for i,point in enumerate(data):
+        if(point>=10.0):
+            return i
+
+
+
+def linearFitDelays(ROpos,outputFile,title):
+
+    c1Data = getTimeDelays("Chip1_filtered.csv") #return clock0,clock4,inverter0,inverter4,notand0,notand4,notor0,notor4,doses,temperatures
+    c2Data = getTimeDelays("Chip2_filtered.csv")
+    c3Data = getTimeDelays("Chip3_filtered.csv")
+    c4Data = getTimeDelays("Chip4_filtered.csv")
+    c6Data = getTimeDelays("Chip6_filtered.csv")
+
+
+    c1x = c1Data[-2]
+    c1y = c1Data[ROpos]
+    c2x = c2Data[-2]
+    c2y = c2Data[ROpos]
+    c3x = c3Data[-2]
+    c3y = c3Data[ROpos]
+    c4x = c4Data[-2]
+    c4y = c4Data[ROpos]
+    c6x = c6Data[-2]
+    c6y = c6Data[ROpos]
+
+    skipC1 = find10Mrad(c1x)
+    skipC2 = find10Mrad(c2x)
+    skipC3 = find10Mrad(c3x)
+    skipC4 = find10Mrad(c4x)
+    skipC6 = find10Mrad(c6x)
+
+    c1Temp = round(np.mean(c1Data[-1]))
+    c2Temp = round(np.mean(c2Data[-1]))
+    c3Temp = round(np.mean(c3Data[-1]))
+    c4Temp = round(np.mean(c4Data[-1]))
+    c6Temp = round(np.mean(c6Data[-1]))
+
+
+    coef1 = np.polyfit(c1x[skipC1:],c1y[skipC1:],1)
+    coef2 = np.polyfit(c2x[skipC2:],c2y[skipC2:],1)
+    coef3 = np.polyfit(c3x[skipC3:],c3y[skipC3:],1)
+    coef4 = np.polyfit(c4x[skipC4:],c4y[skipC4:],1)
+    coef6 = np.polyfit(c6x[skipC6:],c6y[skipC6:],1)
+
+    print("Chip 1 at {0} degrees, slope A = {1:.2g}".format(c1Temp,coef1[0]))
+    print("Chip 2 at {0} degrees, slope A = {1:.2g}".format(c2Temp,coef2[0]))
+    print("Chip 3 at {0} degrees, slope A = {1:.2g}".format(c3Temp,coef3[0]))
+#    print("Chip 4 at {0} degrees, slope A = {1:.2g}".format(c4Temp,coef4[0]))
+#    print("Chip 6 at {0} degrees, slope A = {1:.2g}".format(c6Temp,coef6[0]))
+
+    fn1 = np.poly1d(coef1) 
+    fn2 = np.poly1d(coef2) 
+    fn3 = np.poly1d(coef3) 
+    fn4 = np.poly1d(coef4) 
+    fn6 = np.poly1d(coef6) 
+    # poly1d_fn is now a function which takes in x and returns an estimate for y
+
+    plt.cla()
+    plt.clf()
+
+
+    markersize = 10
+    markeredgewidth = 1
+
+    plt.plot(c1x[::20],c1y[::20], 'kv', markerfacecolor='none', markersize=markersize, markeredgewidth = markeredgewidth,  label='Slope = {0:.2g}'.format(coef1[0]))
+    plt.plot(c2x[::20],c2y[::20], 'ro', markerfacecolor='none', markersize=markersize, markeredgewidth = markeredgewidth,  label='Slope = {0:.2g}'.format(coef2[0]))
+    plt.plot(c3x[::20],c3y[::20], 'gs', markerfacecolor='none', markersize=markersize, markeredgewidth = markeredgewidth,  label='Slope = {0:.2g}'.format(coef3[0]))
+#    plt.plot(c4x[::50],c4y[::50], 'bo', markerfacecolor='none', markersize=markersize, markeredgewidth = markeredgewidth,  label='Slope = {0:.2g}'.format(coef4[0]))
+#    plt.plot(c6x[::50],c6y[::50], 'ko', markerfacecolor='none', markersize=markersize, markeredgewidth = markeredgewidth,  label='Slope = {0:.2g}'.format(coef6[0]))
+
+    plt.plot(c1x[skipC1:], fn1(c1x[skipC1:]), '--k')
+    plt.plot(c2x[skipC2:], fn2(c2x[skipC2:]), '--r')
+    plt.plot(c3x[skipC3:], fn3(c3x[skipC3:]), '--g')
+#    plt.plot(c4x[skipC4:], fn4(c4x[skipC4:]), '--k')
+#    plt.plot(c6x[skipC6:], fn6(c6x[skipC6:]), '--k')
+    plt.title(title)
+    plt.legend(loc='best')
+    plt.savefig(outputFile,dpi=200)
+
+def linearFitDelaysRoom(ROpos,outputFile,title):
+
+    c1Data = getTimeDelays("Chip1_filtered.csv") #return clock0,clock4,inverter0,inverter4,notand0,notand4,notor0,notor4,doses,temperatures
+    c2Data = getTimeDelays("Chip2_filtered.csv")
+    c3Data = getTimeDelays("Chip3_filtered.csv")
+    c4Data = getTimeDelays("Chip4_filtered.csv")
+    c6Data = getTimeDelays("Chip6_filtered.csv")
+
+
+    c1x = c1Data[-2]
+    c1y = c1Data[ROpos]
+    c2x = c2Data[-2]
+    c2y = c2Data[ROpos]
+    c3x = c3Data[-2]
+    c3y = c3Data[ROpos]
+    c4x = c4Data[-2]
+    c4y = c4Data[ROpos]
+    c6x = c6Data[-2]
+    c6y = c6Data[ROpos]
+
+    skipC1 = find10Mrad(c1x)
+    skipC2 = find10Mrad(c2x)
+    skipC3 = find10Mrad(c3x)
+    skipC4 = find10Mrad(c4x)
+    skipC6 = find10Mrad(c6x)
+
+    c1Temp = round(np.mean(c1Data[-1]))
+    c2Temp = round(np.mean(c2Data[-1]))
+    c3Temp = round(np.mean(c3Data[-1]))
+    c4Temp = round(np.mean(c4Data[-1]))
+    c6Temp = round(np.mean(c6Data[-1]))
+
+
+    coef1 = np.polyfit(c1x[skipC1:],c1y[skipC1:],1)
+    coef2 = np.polyfit(c2x[skipC2:],c2y[skipC2:],1)
+    coef3 = np.polyfit(c3x[skipC3:],c3y[skipC3:],1)
+    coef4 = np.polyfit(c4x[skipC4:],c4y[skipC4:],1)
+    coef6 = np.polyfit(c6x[skipC6:],c6y[skipC6:],1)
+
+    print("Chip 1 at {0} degrees, slope A = {1:.2g}".format(c1Temp,coef1[0]))
+    print("Chip 2 at {0} degrees, slope A = {1:.2g}".format(c2Temp,coef2[0]))
+    print("Chip 3 at {0} degrees, slope A = {1:.2g}".format(c3Temp,coef3[0]))
+    print("Chip 4 at {0} degrees, slope A = {1:.2g}".format(c4Temp,coef4[0]))
+    print("Chip 6 at {0} degrees, slope A = {1:.2g}".format(c6Temp,coef6[0]))
+
+    fn1 = np.poly1d(coef1) 
+    fn2 = np.poly1d(coef2) 
+    fn3 = np.poly1d(coef3) 
+    fn4 = np.poly1d(coef4) 
+    fn6 = np.poly1d(coef6) 
+    # poly1d_fn is now a function which takes in x and returns an estimate for y
+
+    plt.cla()
+    plt.clf()
+
+
+    markersize = 10
+    markeredgewidth = 1
+
+#    plt.plot(c1x[::20],c1y[::20], 'kv', markerfacecolor='none', markersize=markersize, markeredgewidth = markeredgewidth,  label='Slope = {0:.2g}'.format(coef1[0]))
+#    plt.plot(c2x[::20],c2y[::20], 'ro', markerfacecolor='none', markersize=markersize, markeredgewidth = markeredgewidth,  label='Slope = {0:.2g}'.format(coef2[0]))
+#    plt.plot(c3x[::20],c3y[::20], 'gs', markerfacecolor='none', markersize=markersize, markeredgewidth = markeredgewidth,  label='Slope = {0:.2g}'.format(coef3[0]))
+    plt.plot(c4x[::50],c4y[::50], 'bo', markerfacecolor='none', markersize=markersize, markeredgewidth = markeredgewidth,  label='Slope = {0:.2g}'.format(coef4[0]))
+    plt.plot(c6x[::50],c6y[::50], 'ko', markerfacecolor='none', markersize=markersize, markeredgewidth = markeredgewidth,  label='Slope = {0:.2g}'.format(coef6[0]))
+
+#    plt.plot(c1x[skipC1:], fn1(c1x[skipC1:]), '--k')
+#    plt.plot(c2x[skipC2:], fn2(c2x[skipC2:]), '--r')
+#    plt.plot(c3x[skipC3:], fn3(c3x[skipC3:]), '--g')
+    plt.plot(c4x[skipC4:], fn4(c4x[skipC4:]), '--k')
+    plt.plot(c6x[skipC6:], fn6(c6x[skipC6:]), '--k')
+    plt.title(title)
+    plt.legend(loc='best')
+    plt.savefig(outputFile,dpi=200)
+
+
+# correctedTitles=["clk","inv","nand","nor"]
+# for i, title in enumerate(correctedTitles):
+#     linearFitCorrected(i,"fitsRoom/"+title+".png",title)
+
+# rawTitles=["clk0","clk4","inv0","inv4","nand0","nand4","nor0","nor4"]
+# for i, title in enumerate(rawTitles):
+#     linearFitRaw(i,"fitsRoom/"+title+".png",title)
 
 rawTitles=["clk0","clk4","inv0","inv4","nand0","nand4","nor0","nor4"]
 for i, title in enumerate(rawTitles):
-    linearFitRaw(i,"fitsRoom/"+title+".png",title)
+    linearFitDelays(i,"fitsDelay/"+title+".png",title)
+
+rawTitles=["clk0","clk4","inv0","inv4","nand0","nand4","nor0","nor4"]
+for i, title in enumerate(rawTitles):
+    linearFitDelaysRoom(i,"fitsDelayRoom/"+title+".png",title)
